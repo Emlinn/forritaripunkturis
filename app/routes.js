f692b1654aa54279c11cb6c5d5f16fcf05e8e0a6
Emil Ragnarsson <emilragnars@gmail.com>
2015-03-07 15:40:05 +0000

    Minor change. Breytti um navigation í profile
    Breytti um navigation í profile og lagaði smávæginlegar útlitsgalla

--- app/routes.js
+++ app/routes.js
@@ -129,7 +129,7 @@ module.exports = function(app, passport) {
 	// Users
 	app.get('/:id?', function(req, res) {
 		var id = req.params.id;
-		Test.find({_id: id}, {'local.firstname': 1, 'local.lastname': 1, 'local.birthday': 1, _id: 1}, function(err, show){
+		Test.find({_id: id}, {'local.firstname':1, 'local.lastname':1, 'local.birthday':1, 'local.textColumn':1, _id:1}, function(err, show){
 			if(err) return console.log(err);
 			res.render('users', {
 				user: req.user,
