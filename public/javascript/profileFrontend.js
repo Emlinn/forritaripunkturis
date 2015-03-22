45b5964accf1293e764b0de7d617032659e3ecc2
Sigurbjörn <Lenovo@Lenovo-THINK.(none)>
2015-03-22 13:10:43 +0000

    datepicker fyrir alla browsera

--- public/javascript/profileFrontend.js
+++ public/javascript/profileFrontend.js
@@ -14,14 +14,21 @@ $(document).ready(function() {
 	                '<option value="Annað">Annað</option>'+
 	              '</select>'+
 	              '<input class="col-md-2" name="education">'+
-	              '<input type="month" class="col-md-2" name="startDate" >'+
-	              '<input type="month" class="col-md-2" name="finishDate" >'+
+	              '<input type="text" class="col-md-2 datepicker" name="startDate">'+
+	              '<input type="text" class="col-md-2 datepicker" name="finishDate" >'+
 	              '<select name = "statuss">'+
 	              '<OPTION> </OPTION>'+
 	              '<OPTION>Lokið</OPTION>'+
 	              '<OPTION>Ólokið</OPTION>'+
 	            '</select>'+
 	            '<input type="button" id="virk" class="eyda" value="eyda"></div>');
+		$(function() {
+    		$( ".datepicker" ).datepicker({
+      			changeMonth: true,
+      			changeYear: true,
+      			yearRange: "-100:+0"
+      		});
+    	}); 
 	});
 	$('.eyda').click(function() {
 		console.log("jebb");
@@ -29,13 +36,20 @@ $(document).ready(function() {
 	});
 	$('#submitJob').click(function() {
 		$('#workedJobs').append('<div class="form-group" class="work">'+
-      '<input class="col-md-3" name="job">'+
-      '<input class="col-md-3" name="jobName">'+
-      '<input class="col-md-1" name="jobPerc">'+
-      '<input type="month" class="col-md-2" name="jobStartDate">'+
-      '<input type="month" class="col-md-2" name="jobEndDate">'+
-      '<input type="button" class="eydaJob" value="Eyða"></div>'+
-    '</div> ')
+	      '<input class="col-md-3" name="job">'+
+	      '<input class="col-md-3" name="jobName">'+
+	      '<input class="col-md-1" name="jobPerc">'+
+	      '<input type="text" class="col-md-2 datepicker" name="jobStartDate">'+
+	      '<input type="text" class="col-md-2 datepicker" name="jobEndDate">'+
+	      '<input type="button" class="eydaJob" value="Eyða"></div>'+
+    	  '</div> ');
+		$(function() {
+    		$( ".datepicker" ).datepicker({
+      			changeMonth: true,
+      			changeYear: true,
+      			yearRange: "-100:+0"
+      		});
+    	}); 
 	});
 
 	$('.eydaJob').click(function() {
@@ -54,4 +68,14 @@ $(document).ready(function() {
             '</select>'+
           '</div> ')
 	});
+
+	$(function() {
+    	$( ".datepicker" ).datepicker({
+    		changeMonth: true,
+    		changeYear: true,
+    		yearRange: "-100:+0"
+    	});
+    });
+
 });
+
