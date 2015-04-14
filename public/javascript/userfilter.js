c7096902eb30c91bc8fb5cd8c5d949764eef31cd
Emil Ragnarsson <emilragnars@gmail.com>
2015-04-14 10:52:05 +0000

    Merkingar
    Merkja hvar filter var fenginn.

--- public/javascript/userfilter.js
+++ public/javascript/userfilter.js
@@ -1,3 +1,14 @@
+/**
+ * jQuery Filtrify v0.2
+ * Beautiful advanced tag filtering with HTML5 and jQuery
+ * http://luis-almeida.github.com/filtrify
+ *
+ * Licensed under the MIT license.
+ * Copyright 2012 Luís Almeida
+ * https://github.com/luis-almeida
+ */
+
+
 $(function() {
 	$('#instafilta-field').instaFilta({
 		targets: '.section-item',
